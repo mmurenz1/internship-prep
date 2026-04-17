f931a7f883eb0be6853516c5bebbb4140359d0b9
mmurenz1 <murenzimickel1@gmail.com>
2026-04-16 20:22:04 -0400

    feat: day 1 javascript basics

---
+++ index.js
@@ -0,0 +1,21 @@
+let name = "Mickel";
+
+function printName() {
+    console.log(`Hello, ${name}!`);
+}
+
+
+const greet = () => console.log(`Hello, ${name}!`);
+
+
+let programmingLanguages = ["JavaScript", "TypeScript", "Python"];
+
+let intern = {
+    name: "Mickel",
+    company: "Intuit",
+    role: "Sotwarea Engineer Intern",
+}
+
+for (let i = 0; i < programmingLanguages.length; i++) {
+    console.log(programmingLanguages[i]);
+}
